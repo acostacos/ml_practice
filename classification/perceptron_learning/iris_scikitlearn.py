d04f4d0a62178bb8dc9dd7a56f322ac65e21ea62
Carlo Acosta <acostacarlom@gmail.com>
2024-08-10 22:24:53 +0800

    Finish implementing Perceptron using scikit-learn

--- classification/perceptron_learning/iris_scikitlearn.py
+++ classification/perceptron_learning/iris_scikitlearn.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import ListedColormap
 from sklearn import datasets
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
@@ -38,6 +40,60 @@ def main():
     # Measure metrics
     print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
     # print('Accuracy: %.2f' % ppn.score(X_test_std, y_test))
+
+    # Plot results
+    X_combined_std = np.vstack((X_train_std, X_test_std))
+    y_combined = np.hstack((y_train, y_test))
+    plot_decision_regions(X=X_combined_std,
+                          y=y_combined,
+                          classifier=ppn,
+                          test_idx=range(105, 150))
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+def plot_decision_regions(X: np.ndarray, y: np.ndarray, classifier, test_idx: range = None, resolution: float = 0.02):
+    # Setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # Plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # Plot case samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black')
+
+    # Highlight test samples
+    if test_idx:
+        # Plot all samples
+        X_test, _ = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100,
+                    label='test set')
 
 if __name__ == "__main__":
     main()
